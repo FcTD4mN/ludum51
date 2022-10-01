c1ae8b1f2c110b2be9040f672ec1b5f88478ae4f
noe marius <noe.marius@gmail.com>
2022-10-01 19:46:03 +0200

    fix animations

--- LudumDare51/Assets/PlayerController.cs
+++ LudumDare51/Assets/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1f;
+    public float moveSpeed = 4f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
@@ -38,12 +38,12 @@ public class PlayerController : MonoBehaviour
         {
             bool success = TryMove(movementInput);
 
-            if (!success && movementInput.x > 0)
+            if (!success)
             {
                 success = TryMove(new Vector2(movementInput.x, 0));
 
             }
-            if (!success && movementInput.y > 0)
+            if (!success)
             {
                 success = TryMove(new Vector2(0, movementInput.y));
             }
@@ -68,19 +68,27 @@ public class PlayerController : MonoBehaviour
 
     private bool TryMove(Vector2 direction)
     {
-        int count = rb.Cast(
+        if (direction != Vector2.zero)
+        {
+            int count = rb.Cast(
             direction,
             movementFilter,
             castCollisions,
             moveSpeed * Time.fixedDeltaTime + collisionOffset);
-        if (count == 0)
-        {
-            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
             return false;
         }
+
     }
 }
