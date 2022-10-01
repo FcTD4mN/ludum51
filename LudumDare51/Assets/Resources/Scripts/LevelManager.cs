0da8a7123a5adbdb23b0e94b7db676a92fa04828
D4mN <damien.lehmann6@gmail.com>
2022-10-01 23:33:22 +0200

    Merged origin

--- LudumDare51/Assets/Resources/Scripts/LevelManager.cs
+++ LudumDare51/Assets/Resources/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +8,32 @@ public class LevelManager : MonoBehaviour
 {
     // Timer for every 10 seconds
     private float mGameTime = 10;
+
     // UI Elements
     private GameObject canvas;
     private GameObject gameOverPanel;
     private GameObject mTimer;
 
+    // Reference to Room Loader (Animation)
+    private GameObject RoomLoader;
+
+    // keep a copy of the executing coroutine (timer)
+    private IEnumerator timerCoroutine;
+
     // Start is called before the first frame update
     void OnEnable()
     {
+        // Retrieve GameObjects reference
+        RoomLoader = GameObject.Find("RoomLoader");
+
         // Retrieve Timer UI element
         gameOverPanel = GameObject.Find("GameOverPanel").gameObject;
         gameOverPanel.SetActive(false);
         canvas = canvas = GameObject.Find("Canvas");
         mTimer = canvas.transform.Find("Chrono").gameObject;
         mTimer.GetComponent<TextMeshProUGUI>().text = Utilities.FormatSecondsToMinuteAndSeconds(mGameTime);
+
+        // Timer Coroutine
         UpdateTimer();
     }
 
@@ -28,21 +41,36 @@ public class LevelManager : MonoBehaviour
     {
         // Timer
         mGameTime -= Time.fixedDeltaTime;
+        // use coroutine instead ou round
     }
 
+    public IEnumerator getTimerCoroutine()
+    {
+        return timerCoroutine;
+    }
 
+    // Timer Coroutine
     private void UpdateTimer()
     {
-        StartCoroutine(Utilities.ExecuteAfter(1f, () =>
+        timerCoroutine = Utilities.ExecuteAfter(1f, () =>
         {
-            // Debug.Log(mGameTime);
-            // Debug.Log(Utilities.FormatSecondsToMinuteAndSeconds(mGameTime));
-            mTimer.GetComponent<TextMeshProUGUI>().text = Utilities.FormatSecondsToMinuteAndSeconds(mGameTime);
+            mTimer.GetComponent<TextMeshProUGUI>().text = Utilities.FormatSecondsToMinuteAndSeconds(Mathf.Round(mGameTime));
             if (mGameTime > 0)
+            {
                 UpdateTimer();
+            }
             else
                 Death();
-        }));
+
+        });
+        StartCoroutine(timerCoroutine);
+    }
+
+    private void ResetTimer()
+    {
+        mGameTime = 10;
+        mTimer.GetComponent<TextMeshProUGUI>().text = Utilities.FormatSecondsToMinuteAndSeconds(mGameTime);
+        UpdateTimer();
     }
 
     // What happens if you run of out time (or die)
@@ -56,13 +84,39 @@ public class LevelManager : MonoBehaviour
     public void Retry()
     {
         // Reset timer and hide death screen
-        mGameTime = 10;
-        mTimer.GetComponent<TextMeshProUGUI>().text = Utilities.FormatSecondsToMinuteAndSeconds(mGameTime);
         gameOverPanel.SetActive(false);
+        ResetTimer();
+
         Time.timeScale = 1;
-        UpdateTimer();
 
         // Reset Character & Enemies : TO-DO
+    }
+
+    // Set-up & Load Next Room
+    public void NextRoom()
+    {
+        // Stop Timer
+        StopCoroutine(timerCoroutine);
+        mTimer.SetActive(false);
+
+        // Load Animation
+        RoomLoader rL = RoomLoader.GetComponent<RoomLoader>();
+        rL.LoadNextRoom(1);
+
+        // Load Room Features
+        StartCoroutine(LoadRoom(rL.transitionTime));
+    }
+
+    public IEnumerator LoadRoom(float waitingTime)
+    {
+        // Wait
+        yield return new WaitForSeconds(waitingTime);
+
+        // Reset Timer
+        ResetTimer();
+        mTimer.SetActive(true);
+
+        // Handle enemies spawn - TO DO
     }
 
 }
