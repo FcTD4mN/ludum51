dff52c3bccd59f2bc089a187816c1ecba30638bc
noe marius <noe.marius@gmail.com>
2022-10-01 18:10:48 +0200

    adds animations, fixes collision on x & y

--- LudumDare51/Assets/PlayerController.cs
+++ LudumDare51/Assets/PlayerController.cs
@@ -11,11 +11,15 @@ public class PlayerController : MonoBehaviour
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -32,11 +36,51 @@ public class PlayerController : MonoBehaviour
     {
         if (movementInput != Vector2.zero)
         {
-            int count = rb.Cast(
-            movementInput,
+            bool success = TryMove(movementInput);
+
+            if (!success && movementInput.x > 0)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+            }
+            if (!success && movementInput.y > 0)
+            {
+                success = TryMove(new Vector2(0, movementInput.y));
+            }
+            animator.SetBool("isMoving", success);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        // Set sprite direction
+        if (movementInput.x < 0)
+        {
+
+            spriteRenderer.flipX = true;
+        }
+        else if (movementInput.x > 0)
+        {
+
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        int count = rb.Cast(
+            direction,
             movementFilter,
             castCollisions,
             moveSpeed * Time.fixedDeltaTime + collisionOffset);
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
